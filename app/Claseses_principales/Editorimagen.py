b0db096582cc9a24105d90ab5fa134c52ffa30d0
estebanechavarria <juanestebanechavarria511@gmail.com>
2025-04-05 17:33:24 -0500

    corregir clase EditorImagen

--- app/Claseses_principales/Editorimagen.py
+++ app/Claseses_principales/Editorimagen.py
@@ -1,31 +1,45 @@
-from PIL import Image
 import os
+from PIL.Image import Image
+
+from app.services.historial_cambios import HistorialCambios
+
 
 class EditorImagen:
+
     def __init__(self):
-        self.imagen_original = None
-        self.imagen_editada = None
-        self.historial = []
+        self.imagen_original: Image | None = None
+        self.imagen_editada: Image | None = None
+        self.historial: HistorialCambios = HistorialCambios()
 
-    def validar_formato(self, ruta: str) -> bool:
-        extensiones_validas = ['.jpg', '.jpeg', '.png', '.bmp']
-        _, extension = os.path.splitext(ruta.lower())
-        return extension in extensiones_validas
+    def cargar_imagen(self, ruta: str):
 
-    def cargar_imagen(self, ruta: str) -> bool:
-        if not self.validar_formato(ruta):
-            print("Formato no válido.")
-            return False
-        try:
+        if os.path.exists(ruta):
             imagen = Image.open(ruta)
-            self.imagen_original = imagen
-            self.imagen_editada = imagen.copy()
-            self.historial.clear()  # Reinicia historial
-            print("Imagen cargada correctamente.")
+            self.imagen_original: Image | None = imagen
+            self.imagen_editada: Image | None = imagen.copy()
+            self.historial: HistorialCambios = HistorialCambios()
+            self.historial.guardar_estado(self.imagen_editada)
+            print( f" la imagen fue cargada correctamente " )
             return True
-        except Exception as e:
-            print(f"Error al cargar la imagen: {e}")
+        else:
+            print(f" La imagen no fue cargada, no fue encontrada en la ruta: {ruta}. ")
             return False
 
-    def imagen_disponible(self) -> bool:
-        return self.imagen_editada is not None
+    def mostrar_imagen(self):
+
+        if self.imagen_editada is not None:
+            print(" Mostrando imagen actual...")
+            return self.imagen_editada.show()
+
+
+        else:
+            print(f" No has cargado una imagen ")
+
+
+    def cargar_foto(self, ruta: str):
+        pass
+
+
+
+
+
