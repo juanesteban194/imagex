d0d93490321b50df7098d4110c6fd2c8a71ec188
estebanechavarria <juanestebanechavarria511@gmail.com>
2025-04-06 08:07:35 -0500

    implementación de funciones inicio_recorte, dibujar rectángulo en clase interfaz

--- app/interfaz_grafica/interfaz.py
+++ app/interfaz_grafica/interfaz.py
@@ -10,20 +10,35 @@ class InterfazEditor:
         self.editor = EditorImagen()
         self.ventana = tk.Tk()
         self.ventana.title("Editor de Imágenes")
-        self.ventana.geometry("600x400")
+        self.ventana.geometry("700x500")
 
         # Botón para cargar imagen
         btn_cargar = tk.Button(self.ventana, text = " Cargar Imagen", command = self.cargar_imagen)
-        btn_cargar.pack(pady=10)
+        btn_cargar.pack()
 
         # Área para mostrar imagen
         self.label_imagen = tk.Label(self.ventana, text = " Imagen no cargada ")
         self.label_imagen.pack()
 
+
+        # Canvas para mostrar imagen
+        self.canvas = tk.Canvas(self.ventana, bg="gray", width=400, height=400)
+        self.canvas.pack(pady=10)
+
+        # Variables para selección de recorte
+        self.rect_id = None
+        self.inicio_x = 0
+        self.inicio_y = 0
+
+        # Eventos del mouse en el canvas
+        self.canvas.bind("<ButtonPress-1>", self.inicio_recorte)
+        self.canvas.bind("<B1-Motion>", self.dibujar_rectangulo)
+        self.canvas.bind("<ButtonRelease-1>", self.aplicar_recorte)
+
         self.ventana.mainloop()
 
     def cargar_imagen(self):
-        ruta = filedialog.askopenfilename(filetypes = [("Imágenes", "*.jpg *.jpeg " )])
+        ruta = filedialog.askopenfilename(filetypes = [("Imágenes JPG", "*.jpg *.jpeg " )])
         if ruta:
             self.editor.cargar_imagen(ruta)
             img: ImageTk.PhotoImage = ImageTk.PhotoImage(self.editor.imagen_editada.resize((300, 300)))
@@ -31,3 +46,20 @@ class InterfazEditor:
             self.label_imagen.image = img  #guarda referencia a la imagen o no se mostrará
         else:
             messagebox.showinfo("Aviso", "No se seleccionó ninguna imagen.")
+
+    def mostrar_imagen(self):
+        imagen = self.editor.imagen_editada.resize((400, 400))
+        self.img_tk = ImageTk.PhotoImage(imagen)
+        self.canvas.create_image(0, 0, anchor="nw", image=self.img_tk)
+
+
+    def inicio_recorte(self, evento):
+        self.inicio_x = evento.x
+        self.inicio_y = evento.y
+        self.rect_id = self.canvas.create_rectangle(self.inicio_x, self.inicio_y, self.inicio_x, self.inicio_y, outline="red")
+
+    def dibujar_rectangulo(self, evento):
+        self.canvas.coords(self.rect_id, self.inicio_x, self.inicio_y, evento.x, evento.y)
+
+
+
