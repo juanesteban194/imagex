8270b6dec993b641e9db6dcd45d45931f8099893
Juan Gomez <juanc1306gg@hotmail.com>
2025-04-05 19:53:34 -0500

    Correción clase recorte

--- app/Clase_recorte.py
+++ app/Clase_recorte.py
@@ -5,7 +5,7 @@ class Recorte:
     def recortar(imagen: Image, coordenadas: tuple[int, int, int, int]) -> Image:
 
         if imagen is None:
-            print("⚠ No hay imagen para recortar.")
+            print(" No hay imagen para recortar")
             return None
 
         x1 = coordenadas[0]
@@ -14,10 +14,10 @@ class Recorte:
         y2 = coordenadas[3]
 
         if x1 >= x2 or y1 >= y2:
-            print("❌ Las coordenadas de recorte no son válidas.")
+            print("Las coordenadas de recorte no son válidas")
             return imagen
 
         imagen_recortada = imagen.crop((x1, y1, x2, y2))
 
-        print(f"✂ Imagen recortada desde ({x1}, {y1}) hasta ({x2}, {y2})")
+        print(f" Imagen recortada desde ({x1}, {y1}) hasta ({x2}, {y2})")
         return imagen_recortada
